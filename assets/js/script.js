9e842b2d4ea5e1ee3348e1338d022a541ddeb535
imrajaneesh <imrajaneesh77@gmail.com>
2023-11-04 17:30:34 +0530

    landing page development

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+// Number Loader
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
+
+// Course Slider 01
+
+$('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 2,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
