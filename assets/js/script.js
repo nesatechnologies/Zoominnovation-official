c4b14df611bbf9aef6f883da624d9ddad3ed5289
imrajaneesh <imrajaneesh77@gmail.com>
2023-11-07 16:18:11 +0530

    adding content and pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-// Number Loader
+(function ($) {
+
+  "use strict";
+
+  // Navbat Overlay
+  
+
+
+  // Number Loader
 
 const counters = document.querySelectorAll(".counter");
 
@@ -19,29 +27,86 @@ counters.forEach((counter) => {
 // Course Slider 01
 
 $('.slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 2,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
       }
-    ]
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+// Course Slider 02
+
+$('.slider-2').slick({
+  centerMode: true,
+  centerPadding: '20px',
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+
+
+// init jarallax parallax
+var initJarallax = function () {
+  jarallax(document.querySelectorAll(".jarallax"));
+
+  jarallax(document.querySelectorAll(".jarallax-img"), {
+    keepImg: true,
   });
+}
+
+initJarallax();
+initQuantitySpinner();
+  
+  
+})(jQuery);
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
